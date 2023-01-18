8cc073e61e08e1b6377de9adf9627bc23a1acead
KimTaeYeong <12170548@inha.edu>
2023-01-18 18:46:47 +0900

    day05_mission9.2

--- day05_mission.py
+++ day05_mission.py
@@ -1,5 +1,15 @@
-As = (f'got{i}' for i in range(10))
+def get_odd():
+    for i in range(10):
+        if i % 2 == 0:
+            pass
+        else:
+            yield i
 
-print(type(As))
-for value in As:
-    print(value)
+count = 1
+for i in get_odd() :
+    if count == 3:
+        print(i)
+        break
+    else:
+        count += 1
+
